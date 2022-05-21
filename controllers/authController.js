c94a5ff6ec39284540e87036348e666e9bd836da
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-05-21 16:19:00 -0300

    first commit

---
+++ controllers/authController.js
@@ -0,0 +1,36 @@
+const { Usuarios } = require("../models");
+const bcrypt = require("bcryptjs");
+
+const jwt = require("jsonwebtoken");
+const secret = require("../configs/secret");
+
+const authController = {
+  async login(req, res) {
+    const { email, senha } = req.body;
+
+    const usuario = await Usuarios.findOne({
+      where: {
+        email,
+      },
+    });
+    if (!usuario) {
+      return res.status(400).json("E-mail não cadastrado!");
+    }
+    if (!bcrypt.compareSync(senha, usuario.senha)) {
+      return res.status(401).json("Senha inválida!");
+    }
+    const token = jwt.sign(
+      {
+        id: usuario.id,
+        email: usuario.email,
+        nome: usuario.nome,
+      },
+      secret.key,
+      { expiresIn: "24h" }
+    );
+
+    return res.json(token);
+  },
+};
+
+module.exports = authController;
---
+++ controllers/usuariosController.js
@@ -0,0 +1,29 @@
+const { Usuarios } = require("../models");
+const bcrypt = require("bcryptjs");
+
+const usuariosController = {
+  async registro(req, res) {
+    const { nome, email, senha } = req.body;
+    const novaSenha = bcrypt.hashSync(senha, 10);
+    const usuario = await Usuarios.findOne({
+      where: {
+        email,
+      },
+    });
+    if (usuario) {
+      return res.status(409).json({ mensagemDeErro: "Usuário já cadastrado!" });
+    }
+    let novoUsuario = await Usuarios.create({
+      nome,
+      email,
+      senha: novaSenha,
+    });
+
+    return res.status(201).json({
+      nome: novoUsuario.nome,
+      email: novoUsuario.email,
+    });
+  },
+};
+
+module.exports = usuariosController;
