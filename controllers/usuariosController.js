32a894fa4134e24c987b2214cf41cbce1b05b166
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-05-22 22:03:21 -0300

    fix: check user test

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -5,24 +5,21 @@ const usuariosController = {
   async registro(req, res) {
     const { nomeUsuario, emailUsuario, senhaUsuario } = req.body;
     const novaSenha = bcrypt.hashSync(senhaUsuario, 10);
-    // const usuario = await Usuarios.findOne({
-    //   where: {
-    //     email,
-    //   },
-    // });
-    // if (usuario) {
-    //   return res.status(409).json({ mensagemDeErro: "Usuário já cadastrado!" });
-    // }
-    let novoUsuario = await Usuarios.create({
+    const usuario = await Usuarios.findOne({
+      where: {
+        emailUsuario,
+      },
+    });
+    if (usuario) {
+      return res.status(409).json({ mensagemDeErro: "Usuário já cadastrado!" });
+    }
+    await Usuarios.create({
       nomeUsuario,
       emailUsuario,
       senhaUsuario: novaSenha,
     });
 
-    return res.status(201).json({
-      nome: novoUsuario.nomeUsuario,
-      email: novoUsuario.emailUsuario,
-    });
+    return res.status(201).json("Usuário Cadastrado");
   },
 };
 
