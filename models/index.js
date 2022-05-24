11399cb7625d5b1c01c93ce445062b7218146da2
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-05-24 17:23:42 -0300

    feat: relationship between tables

--- models/index.js
+++ models/index.js
@@ -5,4 +5,15 @@ const Videos = require("./Videos");
 const MetasFinanceiras = require("./MetasFinanceiras");
 const Pontuacoes = require("./Pontuacoes");
 
+Tarefas.hasOne(Pontuacoes);
+Pontuacoes.belongsTo(Tarefas);
+Usuarios.hasOne(Pontuacoes);
+Pontuacoes.belongsTo(Usuarios);
+MetasQualitativas.hasMany(Videos);
+Videos.belongsTo(MetasQualitativas);
+MetasQualitativas.hasMany(MetasFinanceiras);
+MetasFinanceiras.belongsTo(MetasQualitativas);
+Usuarios.hasMany(MetasFinanceiras);
+MetasFinanceiras.belongsTo(Usuarios);
+
 module.exports = { Usuarios, MetasQualitativas, Tarefas, Videos, MetasFinanceiras, Pontuacoes };
