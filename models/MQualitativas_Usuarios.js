993e0bad412096c356f7e9c1b90868e5bd603b31
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-05-25 11:09:41 -0300

    fix: modelo para associação M:N

--- models/MQualitativas_Usuarios.js
+++ models/MQualitativas_Usuarios.js
@@ -5,25 +5,7 @@ const Usuarios = require("./Usuarios");
 
 const MQualitativas_has_Usuarios = db.define(
   "MQualitativas_has_Usuarios",
-  {
-    idUsuarioFK: {
-      type: DataTypes.INTEGER,
-      unique: true,
-      primaryKey: true,
-      references: {
-        model: Usuarios,
-        key: "idUsuario",
-      },
-    },
-    idMetaQualitativaFK: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      references: {
-        model: MetasQualitativas,
-        key: "idMetaQualitativa",
-      },
-    },
-  },
+  {},
   {
     timestamps: false,
     tableName: "metasqualitativas_has_usuarios",
--- models/index.js
+++ models/index.js
@@ -11,8 +11,8 @@ Usuarios.hasOne(Pontuacoes);
 Pontuacoes.belongsTo(Usuarios);
 MetasQualitativas.hasMany(Videos);
 Videos.belongsTo(MetasQualitativas);
-MetasQualitativas.belongsToMany(Usuarios, { through: "MQualitativas_has_Usuarios" });
-Usuarios.belongsToMany(MetasQualitativas, { through: "MQualitativas_has_Usuarios" });
+MetasQualitativas.belongsToMany(Usuarios, { through: MQualitativas_has_Usuarios });
+Usuarios.belongsToMany(MetasQualitativas, { through: MQualitativas_has_Usuarios });
 
 module.exports = {
   Usuarios,
