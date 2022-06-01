8a8399a763bb1be4aa816b3408105eabed7a67f3
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-06-01 18:11:48 -0300

    fix: .env config

--- helpers/transporter.js
+++ helpers/transporter.js
@@ -1,12 +1,13 @@
 const nodemailer = require("nodemailer");
+require("dotenv/config");
 
 // create reusable transporter object using the default SMTP transport
 let transporter = nodemailer.createTransport({
   host: "smtp.mailgun.org",
   port: 587,
   auth: {
-    user: "postmaster@sandbox652c28128a274d1b9d6dc0a961dc0673.mailgun.org",
-    pass: "a3256932288b6980fab8cb7aa076a87e-27a562f9-cf8a182a",
+    user: process.env.MAILGUN_USER,
+    pass: process.env.MAILGUN_PASS,
   },
 });
 
