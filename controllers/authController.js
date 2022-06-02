432d1a23444a9d9b6d99bd10cb21c4419bf0143b
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-06-02 16:23:42 -0300

    fix: try catch for errors

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,27 +7,30 @@ const secret = require("../configs/secret");
 const authController = {
   async login(req, res) {
     const { email, password } = req.body;
+    try {
+      const usuario = await Usuarios.findOne({
+        where: {
+          emailUsuario: email,
+        },
+      });
+      if (!usuario) {
+        return res.status(400).json({ errorMessage: "Senha ou e-mail incorreto!" });
+      }
+      if (!bcrypt.compareSync(password, usuario.senhaUsuario)) {
+        return res.status(401).json({ errorMessage: "Senha ou e-mail incorreto!" });
+      }
+      const token = jwt.sign(
+        {
+          idUsuario: usuario.idUsuario,
+        },
+        secret.key,
+        { expiresIn: "24h" }
+      );
 
-    const usuario = await Usuarios.findOne({
-      where: {
-        emailUsuario: email,
-      },
-    });
-    if (!usuario) {
-      return res.status(400).json({errorMessage: "Senha ou e-mail incorreto!"});
+      return res.status(200).json(token);
+    } catch (error) {
+      res.status(400).send({ error: "Erro ao realizar login, tente novamente!" });
     }
-    if (!bcrypt.compareSync(password, usuario.senhaUsuario)) {
-      return res.status(401).json({errorMessage: "Senha ou e-mail incorreto!"});
-    }
-    const token = jwt.sign(
-      {
-        idUsuario: usuario.idUsuario,
-      },
-      secret.key,
-      { expiresIn: "24h" }
-    );
-
-    return res.json(token);
   },
 };
 
--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -8,25 +8,33 @@ const usuariosController = {
   async registro(req, res) {
     const { name, email, password } = req.body;
     const novaSenha = bcrypt.hashSync(password, 10);
-    const usuario = await Usuarios.findOne({
-      where: {
-        emailUsuario: email,
-      },
-    });
-    if (usuario) {
-      return res.status(409).json({ errorMessage: "Usuário já cadastrado!" });
+    try {
+      const usuario = await Usuarios.findOne({
+        where: {
+          emailUsuario: email,
+        },
+      });
+      if (usuario) {
+        return res.status(409).json({ errorMessage: "Usuário já cadastrado!" });
+      }
+    } catch (error) {
+      res.status(400).send({ error: "Erro ao buscar usuário, tente novamente!" });
     }
-    const usuarioCriado = await Usuarios.create({
-      nomeUsuario: name,
-      emailUsuario: email,
-      senhaUsuario: novaSenha,
-      pontos: 0,
-    });
-    await Tarefas_has_Usuarios.create({
-      TarefaIdTarefas: 4,
-      UsuarioIdUsuario: usuarioCriado.idUsuario,
-    });
-    return res.status(201).json("Usuário Criado!");
+    try {
+      const usuarioCriado = await Usuarios.create({
+        nomeUsuario: name,
+        emailUsuario: email,
+        senhaUsuario: novaSenha,
+        pontos: 0,
+      });
+      await Tarefas_has_Usuarios.create({
+        TarefaIdTarefas: 4,
+        UsuarioIdUsuario: usuarioCriado.idUsuario,
+      });
+      return res.status(201).json("Usuário Criado!");
+    } catch (error) {
+      res.status(400).send({ error: "Erro ao criar usuário, tente novamente!" });
+    }
   },
   async buscarPontos(req, res) {
     const token = req.headers["authorization"];
@@ -43,12 +51,16 @@ const usuariosController = {
     const id = jwt.verify(token, secret.key, (err, decoded) => {
       return decoded.idUsuario;
     });
-    const usuario = await Usuarios.findOne({
-      where: {
-        idUsuario: id,
-      },
-    });
-    return res.json({ name: usuario.nomeUsuario });
+    try {
+      const usuario = await Usuarios.findOne({
+        where: {
+          idUsuario: id,
+        },
+      });
+      return res.json({ name: usuario.nomeUsuario });
+    } catch (error) {
+      res.status(400).send({ error: "Erro ao buscar nome do usuário, tente novamente!" });
+    }
   },
   async passwordRecovery(req, res) {
     const { email } = req.body;
@@ -73,20 +85,25 @@ const usuariosController = {
       to: email, // list of receivers
       subject: "Recuperação de Senha", // Subject line
       text: `Olá! Você solicitou a alteração de senha do app Up Money!
-            Acesse o endereço para alterar sua senha: https://ga-up-money.netlify.app/password-change/${emailToken}`, // plain text body
+            Acesse o endereço para alterar sua senha: https://ga-up-money.netlify.app/password-change/${encodeURIComponent(
+              emailToken
+            )}`, // plain text body
       html: `<p>Olá! Você solicitou a alteração de senha do app Up Money!</p>
-             <p>Acesse o endereço para alterar sua senha: </p><a href="https://ga-up-money.netlify.app/password-change/${emailToken}">https://ga-up-money.netlify.app/password-change/${emailToken}`, // html body
+             <p>Acesse o endereço para alterar sua senha: </p><a href="https://ga-up-money.netlify.app/password-change/${encodeURIComponent(
+               emailToken
+             )}">https://ga-up-money.netlify.app/password-change/${encodeURIComponent(emailToken)}`, // html body
     });
     console.log("Message sent: %s", info.messageId);
-    return res
-      .status(200)
-      .json({ message: "E-mail enviado para alteração de senha!", token: emailToken });
+    return res.status(200).json({
+      message: "E-mail enviado para alteração de senha!",
+      token: encodeURIComponent(emailToken),
+    });
   },
   async passwordChange(req, res) {
     const token = req.headers["authorization"];
     const { password } = req.body;
     const newPassword = bcrypt.hashSync(password, 10);
-    const email = jwt.verify(token, secret.key, (err, decoded) => {
+    const email = jwt.verify(decodeURIComponent(token), secret.key, (err, decoded) => {
       return decoded.emailUsuario;
     });
     await Usuarios.update(
