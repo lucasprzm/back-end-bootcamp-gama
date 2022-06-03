a12a1c7f9976a2afc1a9c9681cd327f8c3b26596
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-06-03 11:11:26 -0300

    fix: try catch for untracked errors

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -65,62 +65,55 @@ const usuariosController = {
   },
   async passwordRecovery(req, res) {
     const { email } = req.body;
-    const usuario = await Usuarios.findOne({
-      where: {
-        emailUsuario: email,
-      },
-    });
-    if (!usuario) {
-      return res.status(400).json({ errorMessage: "Usuário não cadastrado!" });
+    try {
+      const usuario = await Usuarios.findOne({
+        where: {
+          emailUsuario: email,
+        },
+      });
+      if (!usuario) {
+        return res.status(400).json({ errorMessage: "Usuário não cadastrado!" });
+      }
+      const emailToken = jwt.sign(
+        {
+          emailUsuario: usuario.emailUsuario,
+        },
+        secret.key,
+        { expiresIn: "2h" }
+      );
+      await transporter.sendMail({
+        from: '"Up Money" <up.money.gama@gmail.com>',
+        to: email,
+        subject: "Recuperação de Senha",
+        html: `<p>Olá! Você solicitou a alteração de senha do app Up Money!</p>
+               <p>Acesse o endereço para alterar sua senha: </p><a href="${process.env.WEB_URL}/password-change?token=${emailToken}">${process.env.WEB_URL}/password-change?token=${emailToken}</a>`,
+      });
+      return res.status(200).json({
+        message: "E-mail enviado para alteração de senha!",
+      });
+    } catch (error) {
+      res.status(400).send({ error: "Erro ao verificar e-mail, tente novamente!" });
     }
-    // const basicAuthToken = btoa(`${email}:${process.env.RECOVER_PASSWORD_KEY}`);
-    const emailToken = jwt.sign(
-      {
-        emailUsuario: usuario.emailUsuario,
-      },
-      secret.key,
-      { expiresIn: "2h" }
-    );
-
-    let info = await transporter.sendMail({
-      from: '"Up Money" <up.money.gama@gmail.com>',
-      to: email,
-      subject: "Recuperação de Senha",
-      html: `<p>Olá! Você solicitou a alteração de senha do app Up Money!</p>
-             <p>Acesse o endereço para alterar sua senha: </p><a href="${process.env.WEB_URL}/password-change?token=${emailToken}">${process.env.WEB_URL}/password-change?token=${emailToken}</a>`,
-    });
-    console.log("Message sent: %s", info.messageId);
-    return res.status(200).json({
-      message: "E-mail enviado para alteração de senha!",
-      token: emailToken,
-    });
   },
   async passwordChange(req, res) {
     const jwtToken = req.headers["authorization"];
-    const token = jwtToken.split(" ")[1];
-    const email = jwt.verify(token, secret.key, (err, decoded) => {
-      return decoded.emailUsuario;
-    });
-
-    //const decodedToken = atob(token);
-    //const [email, recoverPassword] = decodedToken.split(":");
-    // if (recoverPassword == process.env.RECOVER_PASSWORD_KEY) {
     const { password } = req.body;
-    const newPassword = bcrypt.hashSync(password, 10);
-    const user = await Usuarios.findOne({
-      where: {
-        emailUsuario: email,
-      },
-    });
-    //   if (!user) {
-    //     return res.status(404).json({ message: "Usuário não encontrado!" });
-    //   }
-    await user.update({ senhaUsuario: newPassword });
-
-    return res.status(200).json({ message: "Senha alterada!" });
-    // } else {
-    //   return res.status(401).json({ message: "Token inválido!" });
-    // }
+    try {
+      const token = jwtToken.split(" ")[1];
+      const email = jwt.verify(token, secret.key, (err, decoded) => {
+        return decoded.emailUsuario;
+      });
+      const newPassword = bcrypt.hashSync(password, 10);
+      const user = await Usuarios.findOne({
+        where: {
+          emailUsuario: email,
+        },
+      });
+      await user.update({ senhaUsuario: newPassword });
+      return res.status(200).json({ message: "Senha alterada!" });
+    } catch (error) {
+      res.status(400).send({ error: "Erro ao alterar senha, tente novamente!" });
+    }
   },
 };
 
